5e8a2dd5395c737126841a956c658baf10f6d073
leonbenenati <69274730+leonbenenati@users.noreply.github.com>
2022-10-26 21:33:58 -0300

    Update Webapp_ipca.py

--- Webapp_ipca.py
+++ Webapp_ipca.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from statsmodels.tsa.api import SARIMAX
 from bcb import sgs
-import pickle
+from pmdarima.arima import auto_arima
+import base64
 
 paginas = ['Previsão']
 pagina = st.sidebar.radio('Selecione uma página', paginas)
@@ -13,27 +13,50 @@ pagina = st.sidebar.radio('Selecione uma página', paginas)
 
 
 if pagina == 'Previsão':
-	st.title("Previsão IPCA")
-	st.subheader('By: Leon Emiliano Benenati')
-	
-	if st.button('CLIQUE Aqui para realizar a previsão'):
+	st.title("Previsão IPCA para 12 meses")
+	st.subheader('By Leon')
+	st.markdown('---')
+	if st.button('Clique aqui para realizar a previsão do IPCA para os próximos 12 meses (demora cerca de 2 mim)'):
 		#Importando dados
 		ipca = sgs.get(('IPCA', 433), start="1995-01-01")
 		#ipca.index = pd.DatetimeIndex(ipca.index ,freq='MS')
-
-		#Modelo Sarima
-		sarima= SARIMAX(ipca, order=(1, 1, 2), seasonal_order=(1,0,1,12)).fit()
-		#sarima.forecast(12)
+		## AUTOARIMA
+		modelo = auto_arima(
+    ipca, 
+   trace = True, 
+    stepwise = False, 
+    seasonal=True, 
+    start_p=0, 
+    start_d=0, 
+    start_q=0, 
+    start_P=0,
+    start_D=0, 
+    start_Q=0, 
+    max_p=2,
+    max_d=2, 
+    max_q=2,
+    max_P=2,
+    max_D=2, 
+    max_Q=2,
+    m=12)
 
 		sns.set(style="darkgrid")
 		fig, ax = plt.subplots(figsize = (18,9))
 		plt.title('Previsão IPCA', fontsize=18, fontweight='bold')
-		plt.plot(sarima.forecast(12), marker = 'o')
+		plt.plot(modelo.predict(12), marker = 'o')
 		plt.show()
 		st.pyplot(fig)
 		
-		subm = pd.DataFrame()
-		subm["previsão IPCA"] = sarima.forecast(12)
+		#subm = pd.DataFrame()
+		#subm["previsão IPCA"] = modelo.predict(12)
 		#st.dataframe(ipca)
-		#st.dataframe(subm)
-		st.table(sarima.forecast(12))
+		pred=np.round(modelo.predict(12),decimals = 2)
+		df = pd.DataFrame()
+		df["previsão IPCA (considere duas casas decimais"] = pred
+		st.table(df)
+		#st.table(modelo.predict(12))
+
+	csv = df.to_csv(index=True)
+	b64 = base64.b64encode(csv.encode()).decode()
+	href = f'<a href="data:file/csv;base64,{b64}" download="Arquivo.csv">Clique para salvar a previsão em csv</a>'
+	st.markdown(href, unsafe_allow_html=True)
